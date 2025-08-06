a0d52bf1790bf96f55d3a8957aa2afaeff066900
Ghazal-Aryem <ghazalmariem911@gmail.com>
2025-08-06 14:30:30 +0500

    initial commit - BookStore_management using .Net MVC

---
+++ bOOkStoreManagement/Repositories/Abstract/IAuthorService.cs
@@ -0,0 +1,14 @@
+﻿using bOOkStoreManagement.Models.Domain;
+
+namespace bOOkStoreManagement.Repositories.Abstract
+{
+    public interface IAuthorService
+    {
+        bool Add(Author model);
+        bool Update(Author model);
+        bool Delete(int id);
+        Author FindById(int id);
+        IEnumerable<Author> GetAll();
+    }
+    
+}
